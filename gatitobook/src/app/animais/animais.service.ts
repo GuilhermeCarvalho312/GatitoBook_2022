8831e9701ef17114f78ec6891765e85d2b722975
Guilherme Augusto de Carvalho Silva <guilhermeacarvalhosilva@gmail.com>
2023-02-23 22:53:43 -0300

    Commit - Aula 3: Detalhe do animal

--- gatitobook/src/app/animais/animais.service.ts
+++ gatitobook/src/app/animais/animais.service.ts
@@ -1,11 +1,13 @@
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Observable } from 'rxjs';
+import { Observable, of, throwError } from 'rxjs';
+import { catchError, mapTo } from 'rxjs/operators';
 import { environment } from 'src/environments/environment';
 import { TokenService } from '../autenticacao/token.service';
-import { Animais } from './animais';
+import { Animais, Animal } from './animais';
 
 const API = environment.apiUrl;
+const NOT_MODIFIED = '304';
 
 @Injectable({
   providedIn: 'root',
@@ -16,12 +18,52 @@ export class AnimaisService {
     private tokenService: TokenService
   ) {}
 
+  /**
+   * Retorna uma lista de animais do usuário com base no nome do usuário.
+   * @param nomeDoUsuario O nome do usuário cujos animais serão listados.
+   * @returns Um Observable que emite uma lista de animais do usuário.
+   */
   public listaDoUsuarios(nomeDoUsuario: string): Observable<Animais> {
-    const token = this.tokenService.retornaToken(),
-      headers = new HttpHeaders().append('x-access-token', token);
+    return this.httpClient.get<Animais>(`${API}/${nomeDoUsuario}/photos`);
+  }
 
-    return this.httpClient.get<Animais>(`${API}/${nomeDoUsuario}/photos`, {
-      headers,
-    });
+  /**
+   * Busca um animal específico com base em seu identificador único.
+   * @param id O identificador único do animal a ser buscado.
+   * @returns Um Observable que emite o animal encontrado.
+   */
+  public buscaPorId(id: number): Observable<Animal> {
+    return this.httpClient.get<Animal>(`${API}/photos/${id}`);
+  }
+
+  /**
+   * Exclui um animal específico com base em seu identificador único.
+   * @param id O identificador único do animal a ser excluído.
+   * @returns Um Observable que emite o animal excluído.
+   */
+  public excluirAnimal(id: number): Observable<Animal> {
+    return this.httpClient.delete<Animal>(`${API}/photos/${id}`);
+  }
+
+  /**
+   * Faz uma requisição HTTP do tipo POST para curtir uma foto de um animal.
+   * @param id O identificador único da foto do animal que será curtida.
+   * @returns Um Observable que emite um booleano `true` em caso de sucesso ou um booleano `false` caso a foto já tenha sido curtida anteriormente.
+   */
+  public curtir(id: number): Observable<boolean> {
+    return this.httpClient
+      .post(
+        `${API}/photos/${id}/like`,
+        {},
+        {
+          observe: 'response',
+        }
+      )
+      .pipe(
+        mapTo(true),
+        catchError((error) => {
+          return error.status === NOT_MODIFIED ? of(false) : throwError(error);
+        })
+      );
   }
 }
--- gatitobook/src/app/autenticacao/autenticacao.module.ts
+++ gatitobook/src/app/autenticacao/autenticacao.module.ts
@@ -1,12 +1,36 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
-
+import { HTTP_INTERCEPTORS } from '@angular/common/http';
+import { AutenticacaoInterceptor } from './autenticacao.interceptor';
 
 @NgModule({
   declarations: [],
-  imports: [
-    CommonModule
-  ]
+  imports: [CommonModule],
+  providers: [
+    {
+      provide: HTTP_INTERCEPTORS,
+      useClass: AutenticacaoInterceptor,
+      multi: true,
+    },
+  ],
 })
-export class AutenticacaoModule { }
+export class AutenticacaoModule {}
+
+
+// Esse código é um exemplo de como registrar um interceptor em um módulo em Angular 11. Ele mostra como você pode registrar um interceptor chamado AutenticacaoInterceptor usando a API HTTP_INTERCEPTORS fornecida pelo Angular.
+
+// O código começa com algumas importações necessárias, incluindo o decorator NgModule, o módulo CommonModule, o objeto HTTP_INTERCEPTORS e a classe AutenticacaoInterceptor.
+
+// Em seguida, ele define um novo módulo chamado AutenticacaoModule. O módulo não tem nenhum componente declarado (por isso não há nada dentro do array declarations), mas ele importa o CommonModule.
+
+// A propriedade providers do decorator NgModule é usada para registrar o interceptor. É adicionado um objeto à propriedade providers que fornece informações sobre o interceptor. Este objeto tem três propriedades:
+
+// provide: O objeto HTTP_INTERCEPTORS é fornecido como um valor. Essa propriedade informa ao Angular que estamos registrando um interceptor.
+
+// useClass: A classe AutenticacaoInterceptor é definida como a classe que deve ser usada como o interceptor.
+
+// multi: A propriedade multi é definida como true para indicar que estamos registrando mais de um interceptor.
+
+// Isso significa que estamos registrando um interceptor chamado AutenticacaoInterceptor usando a API HTTP_INTERCEPTORS. Quando a aplicação é executada, o Angular usará automaticamente esse interceptor para todas as solicitações HTTP.
+
+// Em resumo, este código é um exemplo de como registrar um interceptor em um módulo Angular 11. Ele mostra como você pode usar a API HTTP_INTERCEPTORS para registrar o interceptor e como definir as propriedades provide, useClass e multi.
